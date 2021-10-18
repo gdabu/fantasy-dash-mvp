806497a06f435e3b401098f550ac29a144b22b08
geoff <geoffdabu@gmail.com>
2021-10-17 22:10:37 -0700

    updated meta viewport tag for responsiveness

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,7 @@ export default function Home() {
           content="Fantasy Dash | Quickly analyze your NBA fantasy team stats"
         />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <TeamContext>
